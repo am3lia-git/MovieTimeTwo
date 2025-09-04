40f5fde92cd63cfffa85062818b8e65666de2a21
Amelia <amelia@c-riddell.com>
2025-09-04 13:25:10 -0500

    changed views upon submit

---
+++ MovieTimeTwo/tmdbSearch.swift
@@ -0,0 +1,16 @@
+//
+//  tmdbSearch.swift
+//  MovieTimeTwo
+//
+//  Created by Amelia Riddell on 9/4/25.
+//
+
+import SwiftUI
+
+struct tmdbSearch: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+}
